403d538197bc18d5f475edad903955d6d4e54de5
chutiphon-k <chutiphon.ksp@gmail.com>
2020-03-29 11:32:18 +0700

    :bug: Fix bug cannot assign default to nested object (#1)
    * :bug: Fix bug cannot assign default to nested object
    
    * :bookmark: Bump version

--- dist/defaults.util.js
+++ dist/defaults.util.js
@@ -1,7 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const lodash_1 = require("lodash");
+function customizer(currentValue, targetValue) {
+    if (lodash_1.isPlainObject(currentValue)) {
+        return lodash_1.assignWith({}, currentValue, targetValue, customizer);
+    }
+    return currentValue !== null && currentValue !== void 0 ? currentValue : targetValue;
+}
 exports.defaults = (object, source) => {
-    return lodash_1.assignWith({}, object, source, (objValue, srcValue) => (lodash_1.isNil(objValue) ? srcValue : objValue));
+    return lodash_1.assignWith({}, object, source, customizer);
 };
 //# sourceMappingURL=defaults.util.js.map
