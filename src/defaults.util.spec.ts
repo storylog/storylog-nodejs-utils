ed94c131a9382d340b8e9112ab560f1160791ba3
chutiphon-k <chutiphon.ksp@gmail.com>
2020-03-29 11:30:14 +0700

    :bug: Fix bug cannot assign default to nested object

--- src/defaults.util.spec.ts
+++ src/defaults.util.spec.ts
@@ -15,4 +15,9 @@ describe('defaults', () => {
     const actual = defaults({ a: null }, { a: 1 });
     expect(actual).toStrictEqual({ a: 1 });
   });
+
+  it('should assign source properties if missing on nested `object`', () => {
+    const actual = defaults({ a: { b: 1 }, b: 3 }, { a: { c: 2 } });
+    expect(actual).toStrictEqual({ a: { b: 1, c: 2 } });
+  });
 });
--- src/defaults.util.ts
+++ src/defaults.util.ts
@@ -1,8 +1,15 @@
-import { assignWith, isNil } from 'lodash';
+import { assignWith, isPlainObject } from 'lodash';
+
+function customizer(currentValue: any, targetValue: any): boolean {
+  if (isPlainObject(currentValue)) {
+    return assignWith({}, currentValue, targetValue, customizer);
+  }
+  return currentValue ?? targetValue;
+}
 
 export const defaults = <TObject extends object, TSource extends object>(
   object: TObject,
   source: TSource,
 ): TObject & TSource => {
-  return assignWith({}, object, source, (objValue, srcValue) => (isNil(objValue) ? srcValue : objValue));
+  return assignWith({}, object, source, customizer);
 };
